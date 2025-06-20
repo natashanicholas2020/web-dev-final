4646d2f3adb65445a8e347535c4e1a7e1ae2683a
Natasha Nicholas <nnicholas2020@gmail.com>
2025-06-20 19:45:40 -0400

    profile appears when user is logged in

--- src/LoveIsland/Account/index.tsx
+++ src/LoveIsland/Account/index.tsx
@@ -1,16 +1,43 @@
-import { Routes, Route, Navigate } from "react-router";
-import Signin from "./Signin";
+// import { Routes, Route, Navigate } from "react-router";
+// import Signin from "./Signin";
+// import Profile from "./Profile";
+// import Signup from "./Signup";
+
+// export default function Account() {
+//   return (
+//     <div id="wd-account-screen">
+//       <Routes>
+//         <Route path="/" element={<Navigate to="/LoveIsland/Account/Signin" />} />
+//         <Route path="/Signin" element={<Signin />} />
+//         <Route path="/Profile" element={<Profile />} />
+//         <Route path="/Signup" element={<Signup />} />
+//       </Routes>
+//     </div>
+// );}
+
+
+import { Routes, Route, Navigate } from "react-router-dom";
 import Profile from "./Profile";
-import Signup from "./Signup";
+import Signin from "./Signin";
 
 export default function Account() {
+  const token = localStorage.getItem("token");
+
   return (
-    <div id="wd-account-screen">
-      <Routes>
-        <Route path="/" element={<Navigate to="/LoveIsland/Account/Signin" />} />
-        <Route path="/Signin" element={<Signin />} />
-        <Route path="/Profile" element={<Profile />} />
-        <Route path="/Signup" element={<Signup />} />
-      </Routes>
-    </div>
-);}
+    <Routes>
+      {/* When at /LoveIsland/Account */}
+      <Route
+        path="/"
+        element={token ? <Profile /> : <Navigate to="/LoveIsland/Account/Signin" replace />}
+      />
+
+      {/* Signin page */}
+      <Route path="Signin" element={<Signin />} />
+
+      {/* Profile page explicitly */}
+      <Route path="Profile" element={token ? <Profile /> : <Navigate to="/LoveIsland/Account/Signin" replace />} />
+
+      {/* You can add other Account related routes here */}
+    </Routes>
+  );
+}
